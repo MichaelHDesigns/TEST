e568d8cb6529aaf6b9152a75c5fc7b5c620ce1e2
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2019-12-28 20:53:10 -0500

    Create partners_page.php

---
+++ partners_page.php
@@ -0,0 +1,69 @@
+<?php /* Template Name: partners
+*/ ?>
+<!--Header-->
+<?php get_header(); ?>
+<div class="push-div"></div>
+<div class="container">
+    
+    <div class="partners">
+		
+        <div class="jumbotron">
+        <img src="<?php bloginfo('template_directory'); ?>/images/hthlogo-partners.png" style="max-width:100%">
+            <a href="https://hthcoin.github.io/web/" target="_blank">Help The Homeless Coin</a>.
+		<p>	<small><strong>The team at Help The Homeless Coin is dedicated to supporting and focusing on those that need assistance by aiding charities with boots on the ground.
+We will provide help with shelters, food, clothing, job seminars, access to local employers, and rehabilitation to help rebuild the lives of the homeless.
+Every human on Earth has the right to a fair chance in life. But around the world, millions are being left behind – disadvantaged, endangered and deprived of everything they need to thrive. It doesn’t need to be this way. Investing in local and global communities where the poorest people live not only improves their lives but also the broader community. Let us break the vicious cycle of poverty.</strong></small>
+			</p>
+								
+	 </div>
+        
+        <div class="jumbotron">
+        <img src="<?php bloginfo('template_directory'); ?>/images/partner01.png" style="max-width:100%">
+            <a href="http://landforthehomeless.org/" target="_blank">Land For The Homeless</a>.
+			<p>
+				<small><strong>Land for the Homeless is a New Jersey based 501(c)3 public charity, which is working to construct a supportive tiny home community on a plot of land it owns in Central NJ for the homeless to be able to live upon legally and in peace, without fear of unwarranted eviction or harassment. With shelter overcrowding continuing to be a serious issue across New Jersey and America as a whole, this project aims to create a low-cost solution to a very serious local problem with room for national expansion potential. The philosophy behind the project is that “anything is better than a park bench” and that the homeless problem will not be solved by simply trying to brush away those who are unsheltered but rather can be alleviated through the establishment of a well maintained and uplifting transitional environment for those affected to seek refuge and work toward bettering their situation.</strong></small>
+			</p>
+        </div>
+        
+        <div class="jumbotron">
+        <img src="<?php bloginfo('template_directory'); ?>/images/partner02.png" style="max-width:100%">
+            <a href="https://www.salvationarmy.gr/" target="_blank">Salvation Army Of Athens Greece</a>.
+			<p>
+			<small><strong>The Salvation Army is an international Christian movement that operates in 124 countries worldwide. In Greece, the Salvation Army has been operating as a nonprofit company from January 2011, standing by the homeless, elderly, young families with infants and victims of trafficking. It began its activities in Thessaloniki in 2007 and from 2012 in Athens, too.
+The Salvation Army aims to spread the Christian message of God's love to all, as well as extend humanitarian aid, regardless of politics, religious beliefs or race. Its message is based on the Bible. Its ministry is motivated by love for God. Its mission is to preach the gospel of Jesus Christ and meet human needs in His name without discrimination.
+The Salvation Army in Greece is officially recognized as a Non Profit Company based in Thessaloniki</strong></small>
+			</p>
+        </div>
+    
+		<div class="jumbotron">
+        <figure class="wp-block-image"><img src="https://helpthehomelessworldwide.org/wp-content/uploads/2019/03/MSM.png" alt="" class="wp-image-509"/></figure>
+			<a href="https://www.msmmoorefoundation.org" target="_blank">MSM Moore Foundation</a>.
+			<p>
+			<small><strong>We are the children’s charity dedicated to the fight against childhood cancer, children with disabilities and children with deprived educational Background. Ringing the End of Treatment Bell means a child has finished their cancer treatment. A child has been enrolled into school. Schools are being supplied with Study Materials. Disabled Children are happy seeing joy come to them. We fund vital research so that one day every child will survive and can ring the End of Treatment Bell. MSM Moore Foundation is recognized as a NGO in Ghana, Africa</strong></small>
+			</p>
+        </div>
+			<div class="jumbotron">
+        <figure class="wp-block-image"><img src="https://raw.githubusercontent.com/HTHcoin/web/master/images/partners/Grassroots.jpg" alt="" class="wp-image-509"/></figure>
+			<a href="http://www.grassrootscrisis.org/" target="_blank">Grassroots Crisis Intervention</a>.
+			<p>
+			<small><strong>Grassroots operates a 24-hour general crisis hotline and walk-in counseling program, and is the first point of contact in the County for homeless services.  The agency operates the Mobile Crisis Team, in partnership with Humanim, to respond with the Police to psychiatric emergencies and family crises in the community.  Special youth services include the Maryland Crisis Hotline, the Runaway Intervention Program, outreach and educational programs.  Staff also respond to requests from the community for crisis intervention services following a traumatic event.  The Crisis Intervention Service is certified by the American Association of Suicidology.</strong></small>
+			</p>
+        </div>
+		    <div class="jumbotron">
+        <figure class="wp-block-image"><img src="https://raw.githubusercontent.com/HTHcoin/web/master/images/partners/SGQURBAN.png" alt="" class="wp-image-509"/></figure>
+			<a href="http://www.sgqurban.org//" target="_blank">SGQ Urban</a>.
+			<p>
+			<small><strong>SGQ Urban is a philanthropic initiative that has been setup up to address various objectives with a special focus on developing countries and impoverished nations.
+Since 2012, SGQ Urban has come a long way towards achieving some of their objectives. From a partnership starting with 7 communities in one city, Antananarivo. Today, SGQ Urban serves more than 60 communities in three countries.
+</strong></small>
+			</p>
+        </div>
+    </div>
+	<body>
+		<a href="https://smile.amazon.com/ch/83-1698753" target="_blank">Amazon Smile</a>.
+		<div id="amznCharityBanner"><script type="text/javascript">(function() {var iFrame = document.createElement('iframe'); iFrame.style.display = 'none'; iFrame.style.border = "none"; iFrame.width = 310; iFrame.height = 256; iFrame.setAttribute && iFrame.setAttribute('scrolling', 'no'); iFrame.setAttribute('frameborder', '0'); setTimeout(function() {var contents = (iFrame.contentWindow) ? iFrame.contentWindow : (iFrame.contentDocument.document) ? iFrame.contentDocument.document : iFrame.contentDocument; contents.document.open(); contents.document.write(decodeURIComponent("%3Cdiv%20id%3D%22amznCharityBannerInner%22%3E%3Ca%20href%3D%22https%3A%2F%2Fsmile.amazon.com%2Fch%2F83-1698753%22%20target%3D%22_blank%22%3E%3Cdiv%20class%3D%22text%22%20height%3D%22%22%3E%3Cdiv%20class%3D%22support-wrapper%22%3E%3Cdiv%20class%3D%22support%22%20style%3D%22font-size%3A%2018px%3B%20line-height%3A%2021px%3B%20margin-top%3A%2011.5px%3B%20margin-bottom%3A%2011.5px%3B%22%3ESupport%20%3Cspan%20id%3D%22charity-name%22%20style%3D%22display%3A%20inline-block%3B%22%3EHelp%20The%20Homeless%20Worldwide%20A%20Nj%20Nonprofit%20Corporation.%3C%2Fspan%3E%3C%2Fdiv%3E%3C%2Fdiv%3E%3Cp%20class%3D%22when-shop%22%3EWhen%20you%20shop%20at%20%3Cb%3Esmile.amazon.com%2C%3C%2Fb%3E%3C%2Fp%3E%3Cp%20class%3D%22donates%22%3EAmazon%20donates.%3C%2Fp%3E%3C%2Fdiv%3E%3C%2Fa%3E%3C%2Fdiv%3E%3Cstyle%3E%23amznCharityBannerInner%7Bbackground-image%3Aurl(https%3A%2F%2Fm.media-amazon.com%2Fimages%2FG%2F01%2Fx-locale%2Fpaladin%2Fcharitycentral%2Fbanner-background-image._CB309675353_.png)%3Bwidth%3A300px%3Bheight%3A250px%3Bposition%3Arelative%7D%23amznCharityBannerInner%20a%7Bdisplay%3Ablock%3Bwidth%3A100%25%3Bheight%3A100%25%3Bposition%3Arelative%3Bcolor%3A%23000%3Btext-decoration%3Anone%7D.text%7Bposition%3Aabsolute%3Btop%3A20px%3Bleft%3A15px%3Bright%3A15px%3Bbottom%3A100px%7D.support-wrapper%7Boverflow%3Ahidden%3Bmax-height%3A86px%7D.support%7Bfont-family%3AArial%2Csans%3Bfont-weight%3A700%3Bline-height%3A28px%3Bfont-size%3A25px%3Bcolor%3A%23333%3Btext-align%3Acenter%3Bmargin%3A0%3Bpadding%3A0%3Bbackground%3A0%200%7D.when-shop%7Bfont-family%3AArial%2Csans%3Bfont-size%3A15px%3Bfont-weight%3A400%3Bline-height%3A25px%3Bcolor%3A%23333%3Btext-align%3Acenter%3Bmargin%3A0%3Bpadding%3A0%3Bbackground%3A0%200%7D.donates%7Bfont-family%3AArial%2Csans%3Bfont-size%3A15px%3Bfont-weight%3A400%3Bline-height%3A21px%3Bcolor%3A%23333%3Btext-align%3Acenter%3Bmargin%3A0%3Bpadding%3A0%3Bbackground%3A0%200%7D%3C%2Fstyle%3E")); contents.document.close(); iFrame.style.display = 'block';}); document.getElementById('amznCharityBanner').appendChild(iFrame); })(); </script></div>
+	</body>
+
+</div><!--Page Content Closes-->
+<!--Footer-->
+<?php get_footer(); ?>
