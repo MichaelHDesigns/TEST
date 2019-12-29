1d4e24e68657c4b0bc2cdab21f1c54359ea778f8
devilking6105 <43361618+devilking6105@users.noreply.github.com>
2019-12-28 20:54:52 -0500

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,44 @@
+               
+    </div><!--/allcontainer-->
+            
+        <div class="footersticky">
+            <div class="allcontainer">
+<footer>
+            <div class="container">
+                <div class="row">
+                <div class="col-xs-12 col-md-6 pull-left">
+                    Help The Homeless Worldwide A NJ Nonprofit Corporation is a fully registered <strong>501(c)(3)</strong> Nonprofit Corporation, <strong>EIN 83-1698753</strong>.
+                    
+                    </div>
+                    <div class="col-xs-12 col-md-6 pull-right text-right">
+                    Copyright © 2018 · All Rights Reserved · Help The Homeless Worldwide
+                    </div>
+                    
+                    
+                </div>
+                </div>
+                
+            </footer>
+            <div class="bottom">
+                <div class="container">
+                    <div class="row">
+                    <div class="col-xs-12 col-md-10">
+                    <img src="<?php bloginfo('template_directory'); ?>/images/map-marker.svg"> 900 Barnegat Blvd N Unit 2406 Barnegat, New Jersey 08005 &nbsp;  &nbsp; <img src="<?php bloginfo('template_directory'); ?>/images/email.svg"> business@hthcoin.world  &nbsp;  &nbsp; <img src="<?php bloginfo('template_directory'); ?>/images/phone.svg"> 1-848-221-6383
+                    </div>
+                    <div class="col-xs-12 col-md-2 text-right">
+						<a href="https://www.facebook.com/HelptheHomelessCoinOfficial">  <img src="<?php bloginfo('template_directory'); ?>/images/facebook-box.svg"> </a>   <a href="https://www.Twitter.com/HTHCoin"> <img src="<?php bloginfo('template_directory'); ?>/images/twitter-box.svg"> </a>
+                    </div>
+                        </div>
+                    </div>
+                </div>
+                                </div><!--/allcontainer-->
+            </div><!--/footersticky-->
+
+
+
+
+<?php do_action('wp_footer'); ?>
+
+</body>
+
+</html>
